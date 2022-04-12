8cba53c95e76ae6fb0e77e22fc8ac6c8414a34b6
jenadiusnicholaus <ucode888@gmail.com>
2022-04-12 17:35:40 +0300

    add a design

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  
+
   entry: {
     index: './src/index.js',
-    print: './src/print.js',
+
   },
 
   devServer: {
